df5a824636b0128a2e1b65ad7238297cfeeb8d8d
brutos <fabiio212@hotmail.com>
2022-03-17 16:22:27 -0300

    ordenar algumas coisas

---
+++ assert/js/main.js
@@ -0,0 +1,84 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //Quando clicamos em cada nav__link, removemos a classe show-menu
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+   
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionYop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+        
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+            
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+
+
+/*===== SCROLL REVEL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+    
+});
+
+sr.reveal('.home__data, .about__img, .skills_subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about_text, .skills__img', {delay: 400});
+sr.reveal('.home__social-icon', {interval: 200});
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
+
+// /*SCROLL HOME*/
+// sr.reveal('.home__title', {})
+// sr.reveal('.button__title', {delay: 200})
+// sr.reveal('.home__img', {delay: 400})
+// sr.reveal('.home__social-icon', {interval: 200})
+
+// /*SCROLL ABOUT*/
+// sr.reveal('.home__title', {})
+// sr.reveal('.button', {delay: 200})
+// sr.reveal('.home_img', {delay: 400})
+// sr.reveal('.home__social-icon', {interval: 200})
+
+// /*SCROLL SKILLS*/
+// sr.reveal('.skills__subtitle', {})
+// sr.reveal('.skills__text', {delay: 200})
+// sr.reveal('.skills__data', {interval: 200})
+// sr.reveal('.skills__img', {delay: 400})
+
+// /*SCROLL WORK*/
+// sr.reveal('.work__img', {interval: 200})
+
+// /*SCROLL CONTACT*/
+// sr.reveal('.contact__input', {interval: 200})
